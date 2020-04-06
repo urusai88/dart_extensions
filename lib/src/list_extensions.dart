b75ce63be9c9a0a4fec199bc033abb48e2c74656
urusai88 <archon.saratov@gmail.com>
2020-04-06 10:21:07 +0300

    ListExtensions List.random List.randomIndex List.randomMany

---
+++ lib/src/list_extensions.dart
@@ -0,0 +1,28 @@
+import 'dart:math';
+
+extension ListExtensions<T> on List<T> {
+  T random(Random random) {
+    return this[random.nextInt(length)];
+  }
+
+  int randomIndex(Random random) {
+    return random.nextInt(length);
+  }
+
+  List<T> randomMany(int count, Random random) {
+    if (length < count) {
+      throw StateError('List.length < $count');
+    }
+
+    final result = [];
+    var pool = List<T>.of(this);
+
+    for (var i = 0; i < count; ++i) {
+      final idx = pool.randomIndex(random);
+      result.add(pool[idx]);
+      pool.removeAt(idx);
+    }
+
+    return result;
+  }
+}
